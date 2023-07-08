e6a54b9b9ada5af37d47c01c76ad0e7f89fbd923
user8171 <arsf32303@gmail.com>
2023-07-08 13:21:51 +0800

    add subprocess

--- import_.py
+++ import_.py
@@ -5,14 +5,13 @@ import threading
 import types
 import time
 import warnings
+import subprocess
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler, LineBotSdkDeprecatedIn30
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
-from urllib.request import urlretrieve
